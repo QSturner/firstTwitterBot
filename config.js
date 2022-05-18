45d0b4b9c10a190ea7fbb9d1b70f3576709e40e3
Quentin Sturner <40499745+QSturner@users.noreply.github.com>
2022-05-17 21:26:00 -0400

    Finally added the actual files

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+module.exports = {
+    consumer_key: '6aztvFoN5cklbJhHLELZ46Hen',
+    consumer_secret: 'wDyphUlmwEspbWiajNYEGlYZFbCPFy3y7wW2RuDs2KcTgbPfyv',
+    access_token_key: '1140344487971045377-Zyl9ET8jhoYPf32pbNHNOCyibpzibL',
+    access_token_secret: 'f6XijH0AkyR0K4WVnxcST4q3dFfxgV5HBZGqsISqGOTsh'
+  }
